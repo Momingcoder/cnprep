9aab29e208ee58c898ecf390c7a0544b508e0c91
Momingcoder <a398445075@gmail.com>
2016-09-06 08:31:31 +0800

    __init__

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python
 # -*-coding:utf-8-*-
 
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup, find_packages
+
 setup(
   name = 'cnprep',
-  packages = ['cnprep'],
-  version = '0.0.3',
+  version = '0.0.4',
   description = 'A lib for Chinese text preprocessing',
   author = 'MomingCoder',
   author_email = 'a398445075@gmail.com',
   url = 'https://github.com/momingcoder/cnprep', # use the URL to the github repo
-  download_url = 'https://github.com/momingcoder/cnprep/tarball/0.0.3', # I'll explain this in a second
+  license = 'MIT',
   keywords = ['Chinese', 'text', 'preprocess'], # arbitrary keywords
   classifiers = ['Topic :: Text Processing'],
+  packages = find_packages(exclude=['contrib', 'docs', 'tests']),
+  install_requires = ['xpinyin'],
 )
